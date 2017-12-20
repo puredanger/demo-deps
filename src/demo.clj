0bd70e2a70071f5373e2a921b9c0f87549a12789
puredanger <alex.miller@cognitect.com>
2017-12-20 12:47:38 -0600

    remove specs

--- src/demo.clj
+++ src/demo.clj
@@ -11,22 +11,3 @@
 
 (defn days-between [date1 date2]
   (time/in-days (time/interval date1 date2)))
-
-;; specs
-
-(defn datetime? [d]
-  (instance? org.joda.time.DateTime d))
-
-(s/def ::datetime
-  (s/with-gen datetime?
-    #(gen/fmap coerce/from-long
-		  (s/gen (s/int-in (coerce/to-long "1980-01-01") (coerce/to-long "2020-01-01"))))))
-
-(s/fdef tomorrow
-  :args (s/cat :date ::datetime)
-  :ret ::datetime)
-
-(s/fdef days-between
-  :args (s/& (s/cat :date1 ::datetime :date2 ::datetime)
-          (fn [{:keys [date1 date2]}] (time/before? date1 date2)))
-  :ret nat-int?)
